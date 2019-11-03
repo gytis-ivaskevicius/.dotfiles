c207132ca7a7e96c064db863a43ac820e46cc96f
Gytis Ivaskevicius <gytis02.21@gmail.com>
2019-11-03 03:31:34 +0200

    Updated instal script

--- .config/rice/install.sh
+++ .config/rice/install.sh
@@ -1,11 +1,16 @@
 
 
 # Install system packages
-pacman -S --needed --noconfirm - < $HOME/rice/packages/system.pacman.list
-yay -S --needed --noconfirm - < $HOME/rice/packages/system.yay.list
-npm install -g --noconfirm - < $HOME/rice/packages/system.npm.list
+sudo pacman -S --needed --noconfirm - < $HOME/.config/rice/packages/system.pacman.list
+yay -S --needed --noconfirm - < $HOME/.config/rice/packages/system.yay.list
 
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+npm config set prefix "$HOME"'/.local/share/npm'
 # Install development packages
-pacman -S --needed --noconfirm - < $HOME/rice/packages/development.pacman.list
-yay -S --needed --noconfirm - < $HOME/rice/packages/development.yay.list
-npm install -g --noconfirm - < $HOME/rice/packages/development.npm.list
+sudo pacman -S --needed --noconfirm - < $HOME/.config/rice/packages/development.pacman.list
+yay -S --needed --noconfirm - < $HOME/.config/rice/packages/development.yay.list
+npm install -g --noconfirm - < $HOME/.config/rice/packages/development.npm.list
