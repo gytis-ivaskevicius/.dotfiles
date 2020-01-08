50b70e319b935c2d5fd827b6ce357bd799c8b695
Gytis <gytis02.21@gmail.com>
2020-01-08 14:12:31 +0200

    Bunch of ZeniBook changes

--- .config/nvim/config/global.vim
+++ .config/nvim/config/global.vim
@@ -1,4 +1,4 @@
-set number relativenumber
+set number
 set smartcase ignorecase
 
 set smartindent	smarttab
--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,3 @@
 systemctl --user import-environment PATH DISPLAY HOME DBUS_SESSION_BUS_ADDRESS
-systemctl --no-block --user start xsession.target
+systemctl --user start --wait xsession.target
 
